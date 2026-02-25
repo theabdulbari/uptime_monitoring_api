7d41a7274588ec150e9c69e987f073d6bff351d5
abdul bari <m.abdulbari1987@gmail.com>
2026-02-26 00:26:56 +0600

    Create server and listening

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+title: Uptime Monitoring API.
+Author: Abdul Bari.
+Date: 25-02-2026.
+Description: Uptime Monitoring API is a lightweight pure Node.js application that monitors website and API availability. It performs scheduled health checks, tracks response times, and logs uptime/downtime data.
+*/
+
+// Importing the required modules.
+const http = require('http');
+const url = require('url');
+
+
+// module scaffolding.
+const app = {};
+
+// configuration.
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleRequestResponse);
+    server.listen(app.config.port, () => {
+        console.log('server listening port ' + app.config.port);
+    });
+};
+
+
+// handle request response
+app.handleRequestResponse = (req, res) => {
+    // response handle
+    res.end('Hello wowo')
+};
+
+app.createServer();
